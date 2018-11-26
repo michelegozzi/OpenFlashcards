737f2ded47dc8582980833d592b9c0e30e537235
Michele Gozzi <michelegozzi@gmail.com>
2018-11-25 17:55:23 -0800

    db config changed

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,15 +1,14 @@
-/*
-$(document).ready(function () {
-  $("a[data-confirm]").data({
-    "confirm-fade": true,
-    "confirm-title": "Call to Buzz"
-  });
-  $("a[data-method=delete]").data({
-    "confirm-title": "Warning",
-    "confirm-cancel": "Cancel",
-    "confirm-cancel-class": "btn-cancel",
-    "confirm-proceed": "Delete",
-    "confirm-proceed-class": "btn-danger"
-  });
-});
-*/
+
+// $(document).ready(function () {
+  // $("a[data-confirm]").data({
+    // "confirm-fade": true,
+    // "confirm-title": "Call to Buzz"
+  // });
+  // $("a[data-method=delete]").data({
+    // "confirm-title": "Warning",
+    // "confirm-cancel": "Cancel",
+    // "confirm-cancel-class": "btn-cancel",
+    // "confirm-proceed": "Delete",
+    // "confirm-proceed-class": "btn-danger"
+  // });
+// });
